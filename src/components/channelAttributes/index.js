0e8fb45867ec26e5364824c80829dcf76a175066
adarsh-RI <adarsh@theretailinsights.com>
2023-04-28 11:42:58 +0530

    #6 | set up components

---
+++ src/components/channelAttributes/index.js
@@ -0,0 +1,14 @@
+import React, {
+  Fragment,
+  useMemo,
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+} from "react";
+
+const ChannelAttributes = () => {
+  return <>hello attributes</>;
+};
+
+export default ChannelAttributes;
