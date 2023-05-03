b56519aeba116c540789ab243104d1dc6da05da8
SaqlainKhan <saqlain@theretailinsights.com>
2023-05-03 17:34:30 +0530

    feat:/all product UI functanalites

--- src/components/catalog/allProducts/index.js
+++ src/components/catalog/allProducts/index.js
@@ -12,7 +12,8 @@ import edit from "../../../../assets/icons/edit.svg";
 import Image from "next/image";
 
 import styles from "./allProducts.module.css";
-
+import { Tabs, Tab } from "@mui/material";
+import PropTypes from "prop-types";
 import {
   Grid,
   Button,
@@ -20,6 +21,7 @@ import {
   Card,
   CardContent,
   Typography,
+  Checkbox,
 } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -60,19 +62,52 @@ const AllProducts = () => {
 
   /*-----------------Pagination------------------*/
 
+  const [value, setValue] = useState(0);
+
+  const handleNext = () => {
+    setValue((prevValue) => prevValue + 1);
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleNextClick = () => {
+    if (value < 3) {
+      // assuming you have 3 tabs in total
+      setValue(value + 1);
+      // alert(`Moving to ${tabLabels[value + 1]} tab`);
+    }
+  };
+
+  const tabLabels = [
+    "Draft",
+    "Ready for review",
+    "Revalidate",
+    "Ready for publish",
+  ];
+
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <div>{children}</div>}
+      </div>
+    );
+  }
+
   return (
     <>
       <Grid container spacing={0}>
         {/* ------------------------- row 1 ------------------------- */}
         <Grid item xs={12} lg={12}>
-          {/* <Grid item md={4}>
-              <button
-                onClick={() => setShowBrandCreationForm(true)}
-                className={`btn btn-sm ${styles.add_button_text}`}
-              >
-            + Add New
-              </button>
-            </Grid> */}
           <Card sx={{ p: 5 }}>
             <Grid container spacing={2} justifyContent="space-between">
               <Typography variant="h2" className={styles.main_title}>
@@ -84,7 +119,7 @@ const AllProducts = () => {
               </Button>
             </Grid>
             <CardContent>
-              <TableContainer component={Paper}>
+              {/* <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                   <TableHead>
                     <TableRow>
@@ -132,8 +167,8 @@ const AllProducts = () => {
                     ))}
                   </TableBody>
                 </Table>
-              </TableContainer>
-              <Stack spacing={2}>
+              </TableContainer> */}
+              {/* <Stack spacing={2}>
                 <div className={styles.products_pagination}>
                   <Pagination
                     count={10}
@@ -141,7 +176,190 @@ const AllProducts = () => {
                     onChange={handlePaginationChange}
                   />
                 </div>
-              </Stack>
+              </Stack> */}
+              <Box sx={{ maxWidth: { xs: 320, sm: 480 }}}>
+              <Tabs value={value} onChange={handleChange} >
+                <Tab label={tabLabels[0]} />
+                <Tab label={tabLabels[1]} />
+                <Tab label={tabLabels[2]} />
+                <Tab label={tabLabels[3]} />
+              </Tabs>
+              </Box>
+              <TabPanel value={value} index={0} onNext={handleNext}>
+                <Table style={{ margin: "10px 0" }}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>ITEM ID</TableCell>
+                      <TableCell>NAME</TableCell>
+                      <TableCell>CATEGORY</TableCell>
+                      <TableCell>FORMATION</TableCell>
+                      <TableCell>STATUS</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>1</TableCell>
+                      <TableCell>Item 1</TableCell>
+                      <TableCell>Category A</TableCell>
+                      <TableCell>Formation X</TableCell>
+                      <TableCell>Draft</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>2</TableCell>
+                      <TableCell>Item 2</TableCell>
+                      <TableCell>Category B</TableCell>
+                      <TableCell>Formation Y</TableCell>
+                      <TableCell>Draft</TableCell>
+                    </TableRow>
+                    {/* Add more rows as needed */}
+                  </TableBody>
+                </Table>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNextClick}
+                >
+                  READY FOR REVIEW
+                </Button>
+              </TabPanel>
+              <TabPanel value={value} index={1} onNext={handleNext}>
+                <Table style={{ margin: "10px 0" }}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>ITEM ID</TableCell>
+                      <TableCell>NAME</TableCell>
+                      <TableCell>CATEGORY</TableCell>
+                      <TableCell>FORMATION</TableCell>
+                      <TableCell>STATUS</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>1</TableCell>
+                      <TableCell>Item 1</TableCell>
+                      <TableCell>Category A</TableCell>
+                      <TableCell>Formation X</TableCell>
+                      <TableCell>Draft</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>2</TableCell>
+                      <TableCell>Item 2</TableCell>
+                      <TableCell>Category B</TableCell>
+                      <TableCell>Formation Y</TableCell>
+                      <TableCell>Draft</TableCell>
+                    </TableRow>
+                    {/* Add more rows as needed */}
+                  </TableBody>
+                </Table>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNextClick}
+                >
+                  REVALIDATE
+                </Button>
+              </TabPanel>
+              <TabPanel value={value} index={2} onNext={handleNext}>
+                <Table style={{ margin: "10px 0" }}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>ITEM ID</TableCell>
+                      <TableCell>NAME</TableCell>
+                      <TableCell>CATEGORY</TableCell>
+                      <TableCell>FORMATION</TableCell>
+                      <TableCell>STATUS</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>1</TableCell>
+                      <TableCell>Item 1</TableCell>
+                      <TableCell>Category A</TableCell>
+                      <TableCell>Formation X</TableCell>
+                      <TableCell>Revalidate</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell padding="checkbox">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>2</TableCell>
+                      <TableCell>Item 2</TableCell>
+                      <TableCell>Category B</TableCell>
+                      <TableCell>Formation Y</TableCell>
+                      <TableCell>Revalidate</TableCell>
+                    </TableRow>
+                    {/* Add more rows as needed */}
+                  </TableBody>
+                </Table>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNextClick}
+                >
+                  READY FOR PUBLISH
+                </Button>
+              </TabPanel>
+              <TabPanel value={value} index={3} onNext={handleNext}>
+                <Table style={{ margin: "10px 0" }}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>ITEM ID</TableCell>
+                      <TableCell>NAME</TableCell>
+                      <TableCell>CATEGORY</TableCell>
+                      <TableCell>FORMATION</TableCell>
+                      <TableCell>STATUS</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>1</TableCell>
+                      <TableCell>Item 1</TableCell>
+                      <TableCell>Category A</TableCell>
+                      <TableCell>Formation X</TableCell>
+                      <TableCell>Ready for publish</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>2</TableCell>
+                      <TableCell>Item 2</TableCell>
+                      <TableCell>Category B</TableCell>
+                      <TableCell>Formation Y</TableCell>
+                      <TableCell>Ready for publish</TableCell>
+                    </TableRow>
+                    {/* Add more rows as needed */}
+                  </TableBody>
+                </Table>
+                {/* <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNextClick}
+                >
+                  DRAFT
+                </Button> */}
+              </TabPanel>
             </CardContent>
           </Card>
         </Grid>
