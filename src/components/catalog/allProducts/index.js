d08d6583d1ff60750ade8e3818744f7339aedba2
SaqlainKhan <saqlain@theretailinsights.com>
2023-05-08 22:44:31 +0530

    deleted product detail revalidate & updated p_json

--- src/components/catalog/allProducts/index.js
+++ src/components/catalog/allProducts/index.js
@@ -153,10 +153,6 @@ const AllProducts = () => {
     router.push(`/productDetails`);
   }
 
-  function handleRoute() {
-    router.push(`/productDetailRevalidate`);
-  }
-
   const handlePanelClick = () => {
     console.log("Panel clicked");
   };
@@ -442,7 +438,7 @@ const AllProducts = () => {
                               alt="edit"
                               width={30}
                               height={25}
-                              onClick={() => handleRoute()}
+                              // onClick={() => handleRoute()}
                             />
                           </div>
                         </TableRow>
