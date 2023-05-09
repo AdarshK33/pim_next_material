15728b3c5a93374368e24d40d67d161ed32130c8
adarsh-RI <adarsh@theretailinsights.com>
2023-05-09 16:37:01 +0530

    #27 |worked on product details

--- src/components/catalog/allProducts/index.js
+++ src/components/catalog/allProducts/index.js
@@ -164,17 +164,6 @@ const AllProducts = () => {
     console.log("Panel clicked");
   };
 
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     setProgress((prevProgress) =>
-  //       prevProgress >= 100 ? 10 : prevProgress + 10
-  //     );
-  //   }, 800);
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, []);
-
   return (
     <>
       <Grid container>
