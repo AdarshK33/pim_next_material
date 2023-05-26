4af50f5cc01996246417055fe6a3723030b0cb1a
adarsh-RI <adarsh@theretailinsights.com>
2023-05-26 12:02:11 +0530

    #85 | product apis isse fixed

---
+++ pages/api/catalogServiceNew/categoryList.js
@@ -0,0 +1,33 @@
+import { catalogServiceNew } from "../../../utils/axios";
+import withSession from "../../../utils/session";
+
+function handler(req, res) {
+  const { user: { at = "" } = {}, loggedIn } = req.session;
+  const config = {
+    method: "get",
+    url: "/catalog/all_categories",
+    headers: {
+      Authorization: `Bearer ${at}`,
+    },
+  };
+  catalogServiceNew(config)
+    .then((response) => {
+      if (response.status === 200) {
+        res.status(200).json(response.data.result);
+        Promise.resolve();
+      }
+    })
+    .catch((err) => {
+      console.log(
+        "error caught in -> pages/api/catalogServiceNew/category",
+        err
+      );
+      if (err?.response) {
+        const { status = {} } = err?.response;
+        res.status(status).json(err.response.data.error + " " + status);
+      } else res.status(500).json({ message: "something went wrong" });
+      Promise.reject(err);
+    });
+}
+
+export default withSession(handler);
