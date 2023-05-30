3d2d961a493670ac3d8258c835ff75855113d7a0
adarsh-RI <adarsh@theretailinsights.com>
2023-05-31 00:23:13 +0530

    #92 | changes on channel attributes update

--- src/components/catalog/productDetails/index.js
+++ src/components/catalog/productDetails/index.js
@@ -65,6 +65,10 @@ const ProductDetails = (props) => {
   const [checkUpdate, setcheckUpdate] = useState(false);
   const [updateApiCall, setCallApi] = useState(false);
 
+  // console.log(
+  //   catalogServiceNewReducer?.productPimCodeData,
+  //   "catalogServiceNewReducer?.productPimCodeData"
+  // );
   const inputChangeHandler = (e) => {
     setStateInput({
       ...stateInput,
@@ -141,11 +145,11 @@ const ProductDetails = (props) => {
   }, [role, updateApiCall]);
 
   useEffect(() => {
-    if (!catalogServiceNewReducer?.productPimCodeData) {
+    if (!catalogServiceNewReducer?.productPimCodeData?.productDetails) {
       return;
     }
     // mapping the master.modelAttributes for input field
-    const obj = catalogServiceNewReducer?.productPimCodeData;
+    const obj = catalogServiceNewReducer?.productPimCodeData?.productDetails;
     const inputState = new Object();
     Object.entries(obj).map(([key, value]) => {
       value?.attributes.forEach((val) => {
@@ -243,8 +247,7 @@ const ProductDetails = (props) => {
               </>
             ) : role && router.query.tab == "Revalidate" ? (
               <>
-                <Box className={styles.revalidate_Btn}>
-                </Box>
+                <Box className={styles.revalidate_Btn}></Box>
               </>
             ) : (
               <></>
@@ -269,12 +272,11 @@ const ProductDetails = (props) => {
   };
 
   const sectionAccordionSetUpRender = () => {
-    if (!catalogServiceNewReducer?.productPimCodeData) {
+    if (!catalogServiceNewReducer?.productPimCodeData?.productDetails) {
       return;
     }
-    const obj = catalogServiceNewReducer?.productPimCodeData;
+    const obj = catalogServiceNewReducer?.productPimCodeData?.productDetails;
     return Object.entries(obj).map(([key, value]) => {
-
       if (key) {
         return AccordionSetUp(key, value);
       }
@@ -327,7 +329,11 @@ const ProductDetails = (props) => {
                   color="success"
                   component="label"
                   onClick={activateHandler}
-                  disabled={authorities?.ACTIVE_PRODUCTS == 'r' ? true : commentAPICalled}
+                  disabled={
+                    authorities?.ACTIVE_PRODUCTS == "r"
+                      ? true
+                      : commentAPICalled
+                  }
                 >
                   Activate
                 </Button>
@@ -355,65 +361,130 @@ const ProductDetails = (props) => {
               <h1 className={styles.productDetailTitle}>Product Detail View</h1>
             </Grid>
 
-            <div style={{ width: '100%' }}>
+            <div style={{ width: "100%" }}>
               <div className={styles.prodImg}>
                 <img
                   src="https://images.unsplash.com/photo-1607619056574-7b8d3ee536b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1240&q=80"
-                  style={{ width: '100%' }}
+                  style={{ width: "100%" }}
                 />
               </div>
               <div className={styles.prodDetailsDiv}>
                 <h2 className={styles.pimCodeId}>{router.query.PimCodeId}</h2>
                 <div className={styles.manufactureDiv}>
                   <div>
-                    <h3 className={styles.manufacturerDetailHead}>Manufacturer/Marketer</h3>
-                    <h3 className={styles.manufacturerDetailData}>APEX LABORATORIES PVT LTD</h3>
+                    <h3 className={styles.manufacturerDetailHead}>
+                      Manufacturer/Marketer
+                    </h3>
+                    <h3 className={styles.manufacturerDetailData}>
+                      APEX LABORATORIES PVT LTD
+                    </h3>
                   </div>
                   <div>
-                    <h3 className={styles.manufacturerDetailHead}>Manufacturer/Marketer</h3>
-                    <h3 className={styles.manufacturerDetailData}>APEX LABORATORIES PVT LTD</h3>
+                    <h3 className={styles.manufacturerDetailHead}>
+                      Manufacturer/Marketer
+                    </h3>
+                    <h3 className={styles.manufacturerDetailData}>
+                      APEX LABORATORIES PVT LTD
+                    </h3>
                   </div>
                   <div>
-                    <h3 className={styles.manufacturerDetailHead}>Manufacturer/Marketer</h3>
-                    <h3 className={styles.manufacturerDetailData}>APEX LABORATORIES PVT LTD</h3>
+                    <h3 className={styles.manufacturerDetailHead}>
+                      Manufacturer/Marketer
+                    </h3>
+                    <h3 className={styles.manufacturerDetailData}>
+                      APEX LABORATORIES PVT LTD
+                    </h3>
                   </div>
                 </div>
 
                 <div>
                   <h3 className={styles.manufacturerDetailHead}>Description</h3>
-                  <h3 className={styles.manufacturerDetailData}>Give your body a nutritional boost with the multivitamin and multimineral Zincovit Tablet that is specially formulated to support the overall body functioning. The essential vitamins and minerals support the healthy functioning of the heart, nervous system, immune system, etc. It also has Grape Seed extracts that are loaded with antioxidant properties and help in reducing the cell damage caused by free radicals.... READ MORE</h3>
+                  <h3 className={styles.manufacturerDetailData}>
+                    Give your body a nutritional boost with the multivitamin and
+                    multimineral Zincovit Tablet that is specially formulated to
+                    support the overall body functioning. The essential vitamins
+                    and minerals support the healthy functioning of the heart,
+                    nervous system, immune system, etc. It also has Grape Seed
+                    extracts that are loaded with antioxidant properties and
+                    help in reducing the cell damage caused by free radicals....
+                    READ MORE
+                  </h3>
                 </div>
               </div>
 
-              <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-                <div style={{ width: '49%' }}>
-                  <h3 className={styles.manufacturerDetailHead}>Medicinal Benfits</h3>
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div style={{ width: "49%" }}>
+                  <h3 className={styles.manufacturerDetailHead}>
+                    Medicinal Benfits
+                  </h3>
                   <ul>
-                    <li>Helps in strengthening immunity system of the body so that it can fight against infections</li>
-                    <li>Replenishes the body's needsfor essential vitamins and minerals</li>
-                    <li>Good for a speedy recovery after surgery and pregnancy</li>
-                    <li>Maintains a healthy metabolism and improves appetite</li>
-                    <li>Has a balanced nutrional (vitamins and minerals) dose that helps reduce body fatigue</li>
+                    <li>
+                      Helps in strengthening immunity system of the body so that
+                      it can fight against infections
+                    </li>
+                    <li>
+                      Replenishes the body's needsfor essential vitamins and
+                      minerals
+                    </li>
+                    <li>
+                      Good for a speedy recovery after surgery and pregnancy
+                    </li>
+                    <li>
+                      Maintains a healthy metabolism and improves appetite
+                    </li>
+                    <li>
+                      Has a balanced nutrional (vitamins and minerals) dose that
+                      helps reduce body fatigue
+                    </li>
                   </ul>
                 </div>
                 <div className={styles.divider}></div>
-                <div style={{ width: '49%' }}>
+                <div style={{ width: "49%" }}>
                   <div>
-                    <h3 className={styles.manufacturerDetailHead}>Directions for use</h3>
+                    <h3 className={styles.manufacturerDetailHead}>
+                      Directions for use
+                    </h3>
                     <ul>
-                      <li>Helps in strengthening immunity system of the body so that it can fight against infections</li>
-                      <li>Replenishes the body's needsfor essential vitamins and minerals</li>
+                      <li>
+                        Helps in strengthening immunity system of the body so
+                        that it can fight against infections
+                      </li>
+                      <li>
+                        Replenishes the body's needsfor essential vitamins and
+                        minerals
+                      </li>
                     </ul>
                   </div>
 
                   <div>
-                    <h3 className={styles.manufacturerDetailHead}>Safety Information</h3>
+                    <h3 className={styles.manufacturerDetailHead}>
+                      Safety Information
+                    </h3>
                     <ul>
-                      <li>Helps in strengthening immunity system of the body so that it can fight against infections</li>
-                      <li>Replenishes the body's needsfor essential vitamins and minerals</li>
-                      <li>Good for a speedy recovery after surgery and pregnancy</li>
-                      <li>Maintains a healthy metabolism and improves appetite</li>
-                      <li>Has a balanced nutrional (vitamins and minerals) dose that helps reduce body fatigue</li>
+                      <li>
+                        Helps in strengthening immunity system of the body so
+                        that it can fight against infections
+                      </li>
+                      <li>
+                        Replenishes the body's needsfor essential vitamins and
+                        minerals
+                      </li>
+                      <li>
+                        Good for a speedy recovery after surgery and pregnancy
+                      </li>
+                      <li>
+                        Maintains a healthy metabolism and improves appetite
+                      </li>
+                      <li>
+                        Has a balanced nutrional (vitamins and minerals) dose
+                        that helps reduce body fatigue
+                      </li>
                     </ul>
                   </div>
                 </div>
@@ -421,9 +492,16 @@ const ProductDetails = (props) => {
 
               <div>
                 <div>
-                  <h3 className={styles.manufacturerDetailHead}>Key Ingredients</h3>
+                  <h3 className={styles.manufacturerDetailHead}>
+                    Key Ingredients
+                  </h3>
                   <h3 className={styles.manufacturerDetailData}>
-                    Sugar, Microcrystalline Cellulose (460(i)), Vitamins, Talc (553(iii)), Grape Seed Extract, Calcium Carbonate (170(i)), Stabilizer (468), Minerals, Binders (1401, 1202, 1201), Silicon Dioxide (551), Disodium EDTA, Permitted Symbiotic Food Colour(214), Coating Agents (901, 462). ..... READ MORE</h3>
+                    Sugar, Microcrystalline Cellulose (460(i)), Vitamins, Talc
+                    (553(iii)), Grape Seed Extract, Calcium Carbonate (170(i)),
+                    Stabilizer (468), Minerals, Binders (1401, 1202, 1201),
+                    Silicon Dioxide (551), Disodium EDTA, Permitted Symbiotic
+                    Food Colour(214), Coating Agents (901, 462). ..... READ MORE
+                  </h3>
                 </div>
               </div>
             </div>
